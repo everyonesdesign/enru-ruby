c5922401bd2b6303cea022d90815247ef7a9213d
Trambitskiy Yura <tr.yura@yahoo.com>
2015-05-22 02:15:07 +0300

    Add encoding fix, normalized colors

--- enru.rb
+++ enru.rb
@@ -4,10 +4,14 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'colorize'
+require 'cgi'
 
-word = ARGV.join(' ')
+word = ARGV[0]
 
 if word
+
+	#escape word if it's cyrillic of contains some spec symbols
+	word = CGI.escape word
 
 	page = Nokogiri::HTML(open("https://slovari.yandex.ru/#{word}/%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4/"))
 
@@ -21,16 +25,12 @@ if word
 
 			if classAttr
 				if classAttr.include? 'b-translation__text'
-					puts item.to_s.gsub!(/<.*?>/, '').red
+					puts item.to_s.gsub!(/<.*?>/, '').bold
 				elsif classAttr.include? 'b-translation__tr'
-					puts item.to_s.gsub!(/<.*?>/, '').green
-				elsif classAttr.include? 'b-translation__group-title'
-					puts item.to_s.gsub!(/<.*?>/, '').green
+					puts item.to_s.gsub!(/<.*?>/, '').blue
 				end
 			end
 		end
-
-		puts "\n"
 
 		groups.each do |group|
 			group.traverse do |item|
@@ -39,6 +39,9 @@ if word
 				if classAttr
 					if classAttr.include? 'b-translation__translation-words'
 						puts item.to_s.gsub!(/<.*?>/, '')
+					elsif classAttr.include? 'b-translation__group-title'
+						puts "\n"
+						puts item.to_s.gsub!(/<.*?>/, '').yellow
 					end
 				end
 			end
@@ -54,3 +57,5 @@ if word
 else
 
 	puts 'Type a search'.red
+
+end
